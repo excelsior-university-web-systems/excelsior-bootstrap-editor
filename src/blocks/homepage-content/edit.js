16778c9a6dcd6df3d3ecce9c4d6a67b749f7f344
Ethan Lin <elin@excelsior.edu>
2024-08-15 14:46:57 -0500

    remove margin on buttons group

--- src/blocks/homepage-content/edit.js
+++ src/blocks/homepage-content/edit.js
@@ -6,7 +6,7 @@ const TEMPLATE = [
     ['excelsior-bootstrap/iframe-embed', { className: 'float-md-end ms-0 ms-md-3 mb-3 mb-md-0 ratio ratio-16x9 ratio-md-0' }],
     ['core/paragraph', { content: "[This course explores big data's impact on industries through the Internet of Things (IoT). Students study industry strategies for leveraging data in decision-making, and they learn to design data visualizations through processes like data modeling, processing (aggregation, filtering), mapping data to graphical attributes, and strategic visual encoding based on visual perception properties and the task(s) at hand.]" }],
     ['core/paragraph', { content: "To access your course materials, select the Modules option from the navigation menu." }],
-    ['excelsior-bootstrap/buttons', {}, [
+    ['excelsior-bootstrap/buttons', { className: 'mb-0' }, [
         ['excelsior-bootstrap/button', { text: '<i class="bi bi-person-fill" role="presentation" aria-hidden="true">&nbsp;</i> Your Instructor and Peers', href: '#' }]
     ]]
 ];
@@ -23,8 +23,7 @@ export default function Edit() {
                 <InnerBlocks
                     allowedBlocks={ALLOWED_BLOCKS}
                     template={TEMPLATE}
-                    templateLock={false}
-                    renderAppender={() => <InnerBlocks.DefaultBlockAppender />}
+                    templateLock={true}
                 />
             </div>
         </>
