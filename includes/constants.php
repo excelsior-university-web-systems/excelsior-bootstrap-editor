4cc34eb23c8c33914ea3bb325791b05dc5f24a14
Lin87 <ethan.lin.05@gmail.com>
2024-10-24 11:10:00 -0500

    updated version number to 1.0.3

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,7 @@
 <?php
 namespace ExcelsiorBootstrapEditor;
 
-const XCLSR_BTSTRP_EDITOR_VERSION = '1.0.1';
+const XCLSR_BTSTRP_EDITOR_VERSION = '1.0.3';
 const XCLSR_BTSTRP_EDITOR_PREFIX = 'excelsior-bootstrap-editor';
 const XCLSR_BTSTRP_POST_TYPE_NAME = 'Excelsior Bootstrap Editor';
 const XCLSR_BTSTRP_POST_TYPE = 'excelsior_bootstrap';
