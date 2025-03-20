dd43aa9f202370d1ea522b400aa65dc644b5edc6
Lin87 <ethan.lin.05@gmail.com>
2025-03-20 15:10:51 -0500

    allow iframe embed block in callout and tipbox

--- src/blocks/tipbox/allowed-blocks.js
+++ src/blocks/tipbox/allowed-blocks.js
@@ -6,6 +6,7 @@ export const ALLOWED_BLOCKS = [
     XCLSR_BTSTRP_EDITOR_PREFIX + "/image",
     XCLSR_BTSTRP_EDITOR_PREFIX + "/code-block",
     XCLSR_BTSTRP_EDITOR_PREFIX + "/task-group",
+    XCLSR_BTSTRP_EDITOR_PREFIX + "/iframe-embed",
     "core/heading",
     "core/paragraph",
     "core/list"
