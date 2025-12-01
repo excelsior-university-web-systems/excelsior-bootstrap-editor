c298416f4d667097aaeb9edbea33999a5485f6b3
Lin87 <ethan.lin.05@gmail.com>
2025-12-01 09:53:57 -0600

    removed unused hybrid patterns

--- includes/patterns.php
+++ includes/patterns.php
@@ -21,12 +21,6 @@ add_action( 'current_screen', function() {
             array( 'label' => __( XCLSR_BTSTRP_LABEL.' - Online', XCLSR_BTSTRP_POST_TYPE ) )
         );
 
-        // Register pattern category for Excelsior Bootstrap - Hybrid
-        register_block_pattern_category(
-            XCLSR_BTSTRP_EDITOR_PREFIX.'-hybrid-patterns',
-            array( 'label' => __( XCLSR_BTSTRP_LABEL.' - Hybrid', XCLSR_BTSTRP_POST_TYPE ) )
-        );
-
         // Register patterns
         $patternDirectory = plugin_dir_path( __FILE__ ) . 'patterns/';
         $excelsiorEditorOnlinePatterns = array(
@@ -42,24 +36,6 @@ add_action( 'current_screen', function() {
             array( 'slug' => 'modulereflection', 'title' => 'Module Reflection', 'file' => 'online-module-reflection.html' ),
             array( 'slug' => 'instructornotes', 'title' => 'Instructor Notes', 'file' => 'online-instructor-notes.html' )
         );
-
-        // $excelsiorEditorHybridPatterns = array(
-        //     array( 'slug' => 'hybrid-homepage', 'title' => 'Homepage (Hybrid)', 'file' => 'hybrid-home.html' ),
-        //     array( 'slug' => 'hybrid-overview', 'title' => 'Overview (Hybrid)', 'file' => 'hybrid-overview.html' ),
-        //     array( 'slug' => 'hybrid-lesson', 'title' => 'Lesson (Hybrid)', 'file' => 'hybrid-lesson.html' ),
-        //     array( 'slug' => 'hybrid-lessonwithcards', 'title' => 'Lesson with Cards (Hybrid)', 'file' => 'hybrid-lesson-with-cards.html' ),
-        //     array( 'slug' => 'hybrid-lessonwithmedia', 'title' => 'Lesson with Media (Hybrid)', 'file' => 'hybrid-lesson-with-media.html' ),
-        //     array( 'slug' => 'hybrid-discussion', 'title' => 'Discussion (Hybrid)', 'file' => 'hybrid-discussion.html' ),
-        //     array( 'slug' => 'hybrid-discussion-check-in', 'title' => 'Discussion Check-In (Hybrid)', 'file' => 'hybrid-discussion-check-in.html' ),
-        //     array( 'slug' => 'hybrid-discussion-help', 'title' => 'Discussion Course Help (Hybrid)', 'file' => 'hybrid-discussion-help.html' ),
-        //     array( 'slug' => 'hybrid-assignment', 'title' => 'Assignment (Hybrid)', 'file' => 'hybrid-assignment.html' ),
-        //     array( 'slug' => 'hybrid-quizknowledgecheck', 'title' => 'Quiz / Knowledge Check (Hybrid)', 'file' => 'hybrid-quiz-knowledge-check.html' ),
-        //     array( 'slug' => 'hybrid-modulereflection', 'title' => 'Module Reflection / Key Takeaways (Hybrid)', 'file' => 'hybrid-module-reflection.html' ),
-        //     array( 'slug' => 'hybrid-instructornotes', 'title' => 'Instructor Notes (Hybrid)', 'file' => 'hybrid-instructor-notes.html' ),
-        //     array( 'slug' => 'hybrid-session-sign-up', 'title' => 'Session Sign Up (Hybrid)', 'file' => 'hybrid-session-sign-up.html' ),
-        //     array( 'slug' => 'hybrid-session', 'title' => 'Session (Hybrid)', 'file' => 'hybrid-session.html' ),
-        //     array( 'slug' => 'hybrid-presentation-symposium', 'title' => 'Presentation Symposium (Hybrid)', 'file' => 'hybrid-presentation-symposium.html' )
-        // );
 
         foreach ( $excelsiorEditorOnlinePatterns as $pattern ) {
 
